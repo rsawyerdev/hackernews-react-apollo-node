d33854528ec81911c550cb63e30868dbbe281baa
Rose Sawyer <rosesawyer@Roses-MacBook-Air.local>
2020-02-16 13:19:24 -0500

    backend added

---
+++ server/src/resolvers/Subscription.js
@@ -0,0 +1,26 @@
+function newLinkSubscription(parent, args, context, info) {
+    return context.prisma.$subscribe.link({ mutation_in: ['CREATED' ] }).node()
+}
+
+const newLink = {
+    subscribe: newLinkSubscription,
+    resolve: payload => {
+        return payload
+    }
+}
+
+function newVoteSubscription(parent, args, context, info) {
+    return context.prisma.$subscribe.vote({ mutation_in: ['CREATED '] }).node()
+}
+
+const newVote = {
+    subscribe: newVoteSubscription,
+    resolve: payload => {
+        return payload
+    }
+}
+
+module.exports = {
+    newLink,
+    newVote
+}
